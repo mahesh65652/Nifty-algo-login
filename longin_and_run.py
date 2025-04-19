d293a2c04c750b7899364f856fb281f54147305d
Mahesh ramavat <Mramavat52@gmail.com>
2025-04-19 23:41:27 +0530

    Update longin_and_run.py

--- longin_and_run.py
+++ longin_and_run.py
@@ -1,1 +1,23 @@
-.
+from SmartApi import SmartConnect
+import pyotp
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+client_id = os.getenv("CLIENT_ID")
+pwd = os.getenv("PASSWORD")
+totp_secret = os.getenv("TOTP_SECRET")
+
+# Generate TOTP
+totp = pyotp.TOTP(totp_secret).now()
+
+# Initialize SmartAPI
+obj = SmartConnect(api_key=api_key)
+data = obj.generateSession(client_id, pwd, totp)
+
+print("Longin Success!")
+print("Access Token:", data['data']['access_token'])
+
