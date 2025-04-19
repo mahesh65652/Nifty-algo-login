8b9be90af9458525190b63805ee017c71f02ef0d
Mahesh ramavat <Mramavat52@gmail.com>
2025-04-20 00:39:18 +0530

    Add files via upload

---
+++ algo_longin_and_run.py
@@ -0,0 +1,20 @@
+
+from SmartApi import SmartConnect
+import pyotp
+import os
+
+# Environment variables from GitHub Secrets
+api_key = os.getenv("API_KEY")
+client_id = os.getenv("CLIENT_ID")
+pwd = os.getenv("PASSWORD")
+totp_secret = os.getenv("TOTP_SECRET")
+
+# Generate TOTP
+totp = pyotp.TOTP(totp_secret).now()
+
+# Initialize SmartAPI
+obj = SmartConnect(api_key=api_key)
+data = obj.generateSession(client_id, pwd, totp)
+
+print("Login Success!")
+print("Access Token:", data['data']['access_token'])
